fb588d12f748eb478d04b77386b6731e4879d9c0
faiz <faiz.ramadlan@is-gs.com>
2026-02-28 02:49:36 +0700

    fix: remove auth on freebies

--- routes/misc.js
+++ routes/misc.js
@@ -6,7 +6,6 @@ router.get('/memes', MiscController.getMemes);
 
 // Note: The original index.js did not use authentication on /freebies, 
 // but counter.js passes the access_token. Let's make it authenticated.
-router.use(authentication);
 router.get('/freebies', MiscController.getFreebies);
 
 module.exports = router;
